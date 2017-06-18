c1b50b27e7b76b53164d63d1784deb36e211ba9f
Zhou <theodoruszq@gmail.com>
2017-06-18 22:51:22 +0800

    :add: class basic concepts

---
+++ blog-python/oop/04_mydog.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017-06-18 Sydney <theodoruszq@gmail.com>
+
+"""
+"""
+
+class MyDog(object):
+    def __len__(self):
+        return 100
+
+dog = MyDog()
+print (len(dog))
